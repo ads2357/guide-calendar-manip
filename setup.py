e56d313c00bbb27cf578e1e55dff0a03e884824f
Anthony Stathers <astathers@gmail.com>
2016-09-28 22:28:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='guidediary',
+    description='Diary manipulation tools for Dolphin Guide',
+    version='0.0.1',
+    author='Anthony Stathers',
+    author_email='example@example.org',
+    url='https://devnull-as-a-service.com',
+    packages=['guidediary'],
+)
